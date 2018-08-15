ff08ed832491c4e8efc6a70b8f98b11737f0f9a4
Dd <jedendaniel@gmail.com>
2018-08-15 12:39:25 +0200

    Add points counting, let shot go if it is taken before time end but land after

---
+++ Shoot the shot/Shoot the shot/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+    [SerializeField]
+    Player player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player.AcceptCheckPoint(this);
+    }
+}
--- Shoot the shot/Shoot the shot/Assets/Scripts/Player.cs
+++ Shoot the shot/Shoot the shot/Assets/Scripts/Player.cs
@@ -12,24 +12,31 @@ public class Player : MonoBehaviour {
     Text timeText;
     [SerializeField]
     Text resultText;
-    Vector3 resultPosition;
 
     [SerializeField]
     Button startButton;
     Text startButtonText;
-    
-    Vector2 spawnRectStart = new Vector2(-5,2);
-    Vector2 spawnRectEnd = new Vector2(10,6);
+
+    [SerializeField]
+    Transform basketPosition;
+
+    Vector2 spawnRectStart = new Vector2(-11,4);
+    Vector2 spawnRectEnd = new Vector2(11,7);
     float spin = -10;
     float forceMultiplier = 2.75f;
     int leftBound = -20;
     int rightBound = 20;
     int upBound = 20;
-    
-    int points;
-    float timeLeft;
+    int downBound = 2;
+    List<CheckPoint> checkPoints = new List<CheckPoint>();
+    float shootDistance;
+    bool clockTicking;
 
-	void Start () {
+    float points;
+    float timeLeft;
+    private Vector3 resultPosition;
+
+    void Start () {
         Time.timeScale = 0;
         resultPosition = resultText.transform.position;
         resultText.text = "";
@@ -37,27 +44,47 @@ public class Player : MonoBehaviour {
         startButton.onClick.AddListener(StartGame);
     }
 	
-	void Update () {
-        if (Input.GetMouseButtonDown(0))
+	void Update ()
+    {
+        if (clockTicking)
         {
-            Shoot(GetPointWithRaycast());
-        }
-        if (Input.GetMouseButtonDown(1))
-        {
-            SpawnBall();
-        }
-        if (timeLeft > 0)
-        {
-            timeLeft -= Time.deltaTime;
-            timeText.text = timeLeft.ToString();
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot(GetPointWithRaycast());
+            }
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                SpawnBall();
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                EndGame();
+                StartGame();
+            }
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                timeText.text = timeLeft.ToString();
+            }
+            else
+            {
+                timeText.text = 0.ToString();
+                clockTicking = false;
+            }
         }
         else
         {
-            timeText.text = 0.ToString();
-            EndGame();
+            if (ball.velocity.magnitude <= 0)
+                EndGame();
+        }
+
+        if (checkPoints.Count == 2)
+        {
+            CountBasket();
+            UpdateResult();
         }
         if (IsOutOfBounds()) SpawnBall();
-	}
+    }
 
     Vector3 GetPointWithRaycast()
     {
@@ -75,10 +102,17 @@ public class Player : MonoBehaviour {
         ball.velocity = (target - ball.position) * forceMultiplier;
         ball.AddTorque(new Vector3(0, 0, spin));
         ball.useGravity = true;
+        CalculateShootDistance();
+    }
+
+    void CalculateShootDistance()
+    {
+        shootDistance = (basketPosition.position - ball.position).magnitude;
     }
 
     void SpawnBall()
     {
+        checkPoints.Clear();
         ball.transform.position = new Vector3(
             Random.Range(spawnRectStart.x, spawnRectEnd.x), Random.Range(spawnRectStart.y, spawnRectEnd.y),0);
         ball.velocity = Vector2.zero;
@@ -87,28 +121,49 @@ public class Player : MonoBehaviour {
 
     void StartGame()
     {
+        clockTicking = true;
         startButton.gameObject.SetActive(false);
         resultText.transform.position = resultPosition;
+        points = 0;
         resultText.text = "Result: " + points.ToString();
         SpawnBall();
         Time.timeScale = 1;
-        timeLeft = 10;
+        timeLeft = 30;
         points = 0;
     }
 
     void EndGame()
     {
+        clockTicking = false;
         Time.timeScale = 0;
-        resultText.transform.position = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2);
+        resultText.transform.position = new Vector3(Camera.main.pixelWidth/2, Camera.main.pixelHeight/2);
         startButton.gameObject.SetActive(true);
     }
     
     bool IsOutOfBounds()
     {
-        if (ball.position.x < leftBound
-            || ball.position.y > rightBound
-            || ball.position.z > upBound)
+        if (ball.position.x < leftBound || 
+            ball.position.y > upBound || 
+            ball.position.y < downBound || 
+            ball.position.z > rightBound)
             return true;
         return false;
+    }
+
+    public void AcceptCheckPoint(CheckPoint checkPoint)
+    {
+        if (!checkPoints.Contains(checkPoint))
+            checkPoints.Add(checkPoint);
+    }
+
+    void CountBasket()
+    {
+        points += Mathf.Ceil(shootDistance);
+        
+    }
+
+    void UpdateResult()
+    {
+        resultText.text = "Result: " + points.ToString();
     }
 }
